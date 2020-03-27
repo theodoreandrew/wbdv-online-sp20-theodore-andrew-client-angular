c81e3baa19ee0e1a9825627740e68b60bf9462c3
Theodore Andrew <andrew.t@husky.neu.edu>
2020-03-27 15:11:40 -0700

    remove unused import statement

--- src/app/course-manager/course-manager.module.ts
+++ src/app/course-manager/course-manager.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CourseManagerRoutingModule } from './course-manager-routing.module';
-import { CourseTableComponent } from './course-table/course-table.component';
-import { CourseGridComponent } from './course-grid/course-grid.component';
-import { AddCourseBarComponent } from './add-course-bar/add-course-bar.component';
-import { CourseManagerHomeComponent } from './course-manager-home/course-manager-home.component';
-
+import { CourseManagerRoutingModule } from "./course-manager-routing.module";
+import { CourseTableComponent } from "./course-table/course-table.component";
+import { CourseGridComponent } from "./course-grid/course-grid.component";
+import { CourseManagerHomeComponent } from "./course-manager-home/course-manager-home.component";
 
 @NgModule({
-  declarations: [CourseTableComponent, CourseGridComponent, AddCourseBarComponent, CourseManagerHomeComponent],
-  imports: [
-    CommonModule,
-    CourseManagerRoutingModule
-  ]
+  declarations: [
+    CourseTableComponent,
+    CourseGridComponent,
+    CourseManagerHomeComponent
+  ],
+  imports: [CommonModule, CourseManagerRoutingModule]
 })
-export class CourseManagerModule { }
+export class CourseManagerModule {}

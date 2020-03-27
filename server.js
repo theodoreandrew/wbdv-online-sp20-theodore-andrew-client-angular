308d10b83633c78d69c825e43294dfbc360b6745
Theodore Andrew <andrew.t@husky.neu.edu>
2020-03-27 15:05:21 -0700

    add configuration to package.json

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(
+  express.static(
+    __dirname + "/dist/wbdv-online-sp20-theodore-andrew-client-angular"
+  )
+);
+
+app.get("/*", function(req, res) {
+  res.sendFile(
+    path.join(
+      __dirname +
+        "/dist/wbdv-online-sp20-theodore-andrew-client-angular/index.html"
+    )
+  );
+});
+
+app.listen(process.env.PORT || 8080);
